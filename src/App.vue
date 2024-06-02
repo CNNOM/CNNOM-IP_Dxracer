d3ec7828d6ed401f376636f5826b2c699355c55e
Алексей Зенков <zenkov.m700@mail.ru>
2024-06-02 09:37:33 +0300

    [IPD-1] Создана разметка текста и изображения первого экрана

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,11 @@
 <script setup>
 import FirstScreen from '@/components/FirstScreen.vue'
+
 </script>
 
 <template>
-  <h1>dcdsvfds</h1>
-  <first-screen/>
-<!--  <header>-->
 
-<!--    <div class="wrapper">-->
-<!--      <HelloWorld msg="You did it!" />-->
-<!--    </div>-->
-<!--  </header>-->
+  <first-screen/>
 
 </template>
 
--- src/components/FirstScreen.vue
+++ src/components/FirstScreen.vue
@@ -1,15 +1,43 @@
 <template>
-  <div>
-dfjjjjjjjjjjjjjjjjjjjjjjjjjjj
+  <div class="full-screen-block">
+    <div class="text-container">
+      <p>Текст слева</p>
+    </div>
+    <div class="image-container">
+      <img src="@/assets/logo.svg" alt="Изображение справа">
+    </div>
   </div>
 </template>
 
 <script>
 export default {
 
-}
+};
 </script>
 
 <style lang="scss" scoped>
+.full-screen-block {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+}
 
+.text-container {
+  flex: 1;
+  padding: 20px;
+  text-align: left;
+}
+
+.image-container {
+  flex: 1;
+  padding: 20px;
+  text-align: right;
+}
+
+img {
+
+  height: auto;
+}
 </style>
