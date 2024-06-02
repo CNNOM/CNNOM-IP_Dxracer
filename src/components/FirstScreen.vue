28716d5afc24d3f58fbc645bcb4804f45daebf2c
Алексей Зенков <zenkov.m700@mail.ru>
2024-06-02 12:18:21 +0300

    [IPD-1] Добавлено модальное окно для оформления заказа

--- src/components/FirstScreen.vue
+++ src/components/FirstScreen.vue
@@ -1,27 +1,65 @@
 <template>
-  <div class="full-screen-block">
-    <div class="text-container slide-up">
-      <p>Текст слева</p>
+  <header class="page-header">
+    <div class="full-screen-block">
+      <div class="text-container slide-up">
+        <img src="@/assets/DXRacer_Primary_Black_Logo.png">
+        <h1>DXRACER CRA/D5000/N</h1>
+        <p>- геймерское кресло Craft Originals,
+          обитое классической черной эко-кожей с изящной золотой прострочкой.</p>
+        <button class="golden-button"  @click="showModal = true">Оформить заказ</button>
+      </div>
+      <div class="image-container slide-up">
+        <img src="@/assets/D5000N-002.2560.png" alt="Изображение справа">
+      </div>
     </div>
-    <div class="image-container slide-up">
-      <img src="@/assets/logo.svg" alt="Изображение справа">
-    </div>
-  </div>
+    <order-modal v-model:showModal="showModal" />
+
+  </header>
 </template>
 
 <script>
+import OrderModal from '@/components/OrderModal.vue'
 export default {
+  components: {
+    OrderModal,
+  },
+  data() {
+    return {
+      showModal: false
+    };
+  }
 
 };
 </script>
 
 <style lang="scss" scoped>
+
+.page-header {
+  font-family: Noto Sans, sans-serif;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  //background-color: #4a4a4a;
+  box-shadow: 0 5px 30px 0 rgba(0,0,0,0.2); /* Тень справа и слева */
+}
+
 .full-screen-block {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  height: 100vh;
-  width: 100vw;
+  padding: 5vh 5vw; /* Отступы сверху и снизу 5vh, по бокам 5vw */
+  width: 100%;
+  max-width: 1600px; /* Максимальная ширина блока */
+  height: 100%;
+  max-height: 700px;
+  background: url("@/assets/fon-header.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  overflow: hidden;
+  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.3); /* Внутренняя тень */
+  border-radius: 30px;
 }
 
 .text-container,
@@ -35,7 +73,7 @@ export default {
 }
 
 .slide-up {
-  animation: slideUp 2s forwards; /* Запускаем анимацию */
+  animation: slideUp 1.5s forwards; /* Запускаем анимацию */
 }
 
 @keyframes slideUp {
@@ -48,9 +86,92 @@ export default {
     transform: translateY(0);
   }
 }
+.golden-button {
+  background-color: rgba(255, 255, 255, 0.5);
+  border: 3px solid gold;
+  color: gold;
+  padding: 10px 20px;
+  margin-top: 20px;
+  cursor: pointer;
+  font-size: 1.5rem;
+  transition: background-color 0.3s ease, color 0.3s ease;
+  border-radius: 15px ;
+}
+
+.golden-button:hover {
+  background-color: gold;
+  color: white;
+}
+
+.image-container::after {
+  content: '';
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  //background: purple;
+  border: 3px solid #ffd700;
+  transform: skewY(-30deg);
+  z-index: -1;
+  border-radius: 50%;
+}
+.text-container,
+.image-container {
+  flex: 1;
+  padding: 20px;
+  text-align: center;
+  opacity: 0;
+  transform: translateY(20px);
+  transition: all 1s ease-in-out;
+}
 
 img {
-  max-width: 70%;
+  max-width: 100%;
   height: auto;
 }
+
+@media screen and (max-width: 1920px) {
+  .full-screen-block {
+    padding: 5vh 5vw; /* Отступы сверху и снизу 5vh, по бокам 5vw */
+  }
+}
+
+@media screen and (max-width: 1600px) {
+  .full-screen-block {
+    padding: 4vh 4vw; /* Отступы сверху и снизу 4vh, по бокам 4vw */
+  }
+}
+
+@media screen and (max-width: 1280px) {
+  .full-screen-block {
+    padding: 3vh 3vw; /* Отступы сверху и снизу 3vh, по бокам 3vw */
+  }
+}
+
+@media screen and (max-width: 1024px) {
+  .full-screen-block {
+    padding: 2vh 2vw; /* Отступы сверху и снизу 2vh, по бокам 2vw */
+  }
+}
+
+@media screen and (max-width: 768px) {
+  .full-screen-block {
+    padding: 1vh 1vw; /* Отступы сверху и снизу 1vh, по бокам 1vw */
+  }
+}
+
+@media screen and (max-width: 480px) {
+  .full-screen-block {
+    padding: 0.5vh 0.5vw; /* Отступы сверху и снизу 0.5vh, по бокам 0.5vw */
+  }
+}
+
+@media screen and (max-width: 320px) {
+  .full-screen-block {
+    padding: 0.5vh 0.5vw; /* Отступы сверху и снизу 0.5vh, по бокам 0.5vw */
+  }
+}
+
+
 </style>
---
+++ src/components/OrderModal.vue
@@ -0,0 +1,127 @@
+<template>
+  <div v-if="showModal" class="modal">
+    <div class="modal-content">
+      <span class="close" @click="closeModal">&times;</span>
+      <h2>Оформление заказа</h2>
+      <form @submit.prevent="submitForm">
+        <div class="form-group">
+          <label for="firstName">Имя</label>
+          <input type="text" id="firstName" v-model="form.firstName" required>
+        </div>
+        <div class="form-group">
+          <label for="lastName">Фамилия</label>
+          <input type="text" id="lastName" v-model="form.lastName" required>
+        </div>
+        <div class="form-group">
+          <label for="phone">Телефон</label>
+          <input type="tel" id="phone" v-model="form.phone" required>
+        </div>
+        <div class="form-group">
+          <label for="email">Email</label>
+          <input type="email" id="email" v-model="form.email" required>
+        </div>
+        <div class="form-group">
+          <label for="city">Город</label>
+          <input type="text" id="city" v-model="form.city" required>
+        </div>
+        <button type="submit">Отправить</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        firstName: '',
+        lastName: '',
+        phone: '',
+        email: '',
+        city: ''
+      }
+    };
+  },
+  props: {
+    showModal: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    closeModal() {
+      this.$emit('update:showModal', false);
+    },
+    submitForm() {
+      console.log(this.form);
+      this.closeModal();
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.modal {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal-content {
+  background: white;
+  width: 500px;
+  padding: 20px;
+  border-radius: 5px;
+  position: relative;
+}
+
+.close {
+  position: absolute;
+  top: 10px;
+  right: 15px;
+  font-size: 28px;
+  cursor: pointer;
+}
+
+.form-group {
+  margin-bottom: 15px;
+  margin-right: 20px;
+}
+
+.form-group label {
+  display: block;
+  margin-bottom: 5px;
+}
+
+.form-group input {
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+
+button[type="submit"] {
+  display: block;
+  width: 100%;
+  padding: 10px;
+  background-color: rgba(255, 255, 255, 0.5);
+  border: 3px solid gold;
+  color: gold;
+  cursor: pointer;
+  font-size: 1.2rem;
+  border-radius: 15px ;
+
+}
+
+button[type="submit"]:hover {
+  background-color: gold;
+  color: white;
+}
+</style>
