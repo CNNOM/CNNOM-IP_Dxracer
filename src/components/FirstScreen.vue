b21a3f2685fd11dca1050b2554ae4a5cc4ec94fa
Алексей Зенков <zenkov.m700@mail.ru>
2024-06-02 09:57:40 +0300

    [IPD-1] Добавлено плавное появление экран

--- src/components/FirstScreen.vue
+++ src/components/FirstScreen.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="full-screen-block">
-    <div class="text-container">
+    <div class="text-container slide-up">
       <p>Текст слева</p>
     </div>
-    <div class="image-container">
+    <div class="image-container slide-up">
       <img src="@/assets/logo.svg" alt="Изображение справа">
     </div>
   </div>
@@ -24,20 +24,33 @@ export default {
   width: 100vw;
 }
 
-.text-container {
-  flex: 1;
-  padding: 20px;
-  text-align: left;
-}
-
+.text-container,
 .image-container {
   flex: 1;
   padding: 20px;
-  text-align: right;
+  text-align: center;
+  opacity: 0; /* Делаем элементы невидимыми изначально */
+  transform: translateY(20px); /* Сдвигаем элементы вниз */
+  transition: all 1s ease-in-out; /* Добавляем плавную анимацию */
+}
+
+.slide-up {
+  animation: slideUp 2s forwards; /* Запускаем анимацию */
+}
+
+@keyframes slideUp {
+  from {
+    opacity: 0;
+    transform: translateY(50vh);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
 }
 
 img {
-
+  max-width: 70%;
   height: auto;
 }
 </style>
